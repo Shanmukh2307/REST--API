c9458ceb5c829f123124806032f540e3a27d90f6
Shanmukh2307 <shanmukhv78@gmail.com>
2025-03-19 21:45:47 +0530

    Cretaed Initial RestAPI for Goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,35 +1,52 @@
 const asyncHandler= require ('express-async-handler')
 
+const Goal = require ('../models/goalModel')
 
 // @desc   Get Goals
 // @route  GET /api/goals
 // access  private 
-const getGoals= asyncHandler(req,res)=>{
-    res.status(200).json({message:"Get goals"});
-}
+const getGoals= asyncHandler(async(req,res)=>{
+    const goals = await Goal.find()
+
+    res.status(200).json(goals);
+})
 // @desc   Set Goal
 // @route  POST /api/goals
 // access  private 
-const setGoal= asyncHandler(req,res)=>{
+const setGoal= asyncHandler(async(req,res)=>{
     if(!req.body.text){
         res.status(400)
         throw new Error("Please add a text field")
     }
-    console.log(req.body);
-    res.status(200).json({message:"Set goals"})
-}
+    const goal = await Goal.create({
+        text :req.body.text
+    })
+    res.status(200).json(goal)
+})
 // @desc   Update Goal
 // @route  PUT /api/goals/:id
 // access  private 
-const updateGoal= asyncHandler(req,res)=>{
-    res.status(200).json({message:`Update goal ${req.params.id}`})
-}
+const updateGoal= asyncHandler(async(req,res)=>{
+    const goal= await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error ('Goal not found')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate({_id:req.params.id},req.body,{new :true,})
+    res.status(200).json(updatedGoal)
+})
 // @desc   Delete Goal
 // @route  DELETE /api/goals/:id
 // access  private 
-const deleteGoal= asyncHandler(req,res)=>{
-    res.status(200).json({message:`Delete goal ${req.params.id}`})
-}
+const deleteGoal= asyncHandler(async(req,res)=>{
+    const goal= await Goal.findById(req.params.id)
+    if(!goal){
+       res.status(400)
+       throw new Error('Goal not found')
+    }
+    await goal.deleteOne();
+    res.status(200).json({ _id: req.params.id});
+})
 
 module.exports={
     getGoals,
